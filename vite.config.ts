673bfa0a0833085076490ab5063280111f825adb
TYeshua <thiagoyeshua01@gmail.com>
2025-10-15 21:20:21 -0300

    Backup antes de limpar e buildar

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: { exclude: ['lucide-react'] },
-  base: '/Plataforma-LABX/', // ⚠️ com hífen e barra final
+  base: '/Plataforma-LABX/',
+  plugins: [react()]
 })
